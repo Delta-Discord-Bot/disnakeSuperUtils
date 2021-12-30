2107227c4a0835e0a6a7aa831f4717df3f189160
Ariz-z <aakashagrawal194@gmail.com>
2021-12-30 09:50:37 +0530

    updated the version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "disnakeSuperUtils.music.lavalink": ["*"],
     },
     include_package_data=True,
-    version="0.4.0",
+    version="0.5.0",
     license="MIT",
     description="Discord Bot Development made easy!",
     long_description=README,
